3240585c679f0cdcb01469951a35befe9e34c528
Luca Fornerone <forneronejob@gmail.com>
2024-06-06 21:50:19 +0200

    v0.5.0

--- build/config.js
+++ build/config.js
@@ -5,7 +5,7 @@
 
 /** Common */
 const appName = 'sharefi';
-const version = '0.4.1';
+const version = '0.5.0';
 const description = 'Share files through your local network';
 const author = 'Luca Fornerone';
 const completeName = 'sharefi-electron';
